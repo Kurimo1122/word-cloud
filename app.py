57a7f9a9d15ad3bb207a0d9da10f51f7bf5f3775
Kurimo1122 <kugimoto1122@gmail.com>
2016-11-04 17:14:15 +0900

    Modified .env

--- app.py
+++ app.py
@@ -19,14 +19,15 @@ import codecs
 from PIL import Image
 
 """
-# Consumer Key
-CONSUMER_KEY = os.environ['CONSUMER_KEY']
-# Consumer Secret
-CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 CALLBACK_URL = 'http://localhost:5000' # local environment
+"""
+
 # Set key to use session of flask
 app.secret_key = os.environ['SECRET_KEY']
-"""
+# Consumer Secret
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+# Consumer Key
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
 
 # CALLBACK_URL (Will be redirected after authentication)
 CALLBACK_URL = 'https://twitter-word-cloud-toshi.herokuapp.com'
