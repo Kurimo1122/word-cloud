43df0a6067ee06a99b6ffa5c4a04f6ccf1dd2d7a
Kurimo1122 <kugimoto1122@gmail.com>
2016-11-04 16:36:47 +0900

    test

--- app.py
+++ app.py
@@ -44,76 +44,76 @@ def index():
     
     if timeline == False:
         return render_template('index.html', timeline=timeline)
+    else:
+        user_image = timeline[0].user.profile_image_url
+        timeline_list = []   
+        text_list = []
+        wakati_list = []
+        user_image = ""
+        test_list = []
+        text_all = ""
+
+        for status in timeline:
+            text = status.text
+            if 'RT' in text:
+                pass
+            elif '@' in text:
+                pass
+            else:
+                text_list.append(text)
+
+        text_all += "".join(text_list)
     
-    user_image = timeline[0].user.profile_image_url
-    timeline_list = []   
-    text_list = []
-    wakati_list = []
-    user_image = ""
-    test_list = []
-    text_all = ""
+        # keitaiso bunseki
+        tagger = Tagger()
+        wakati_text = tagger.parse(text_all)
 
-    for status in timeline:
-        text = status.text
-        if 'RT' in text:
-            pass
-        elif '@' in text:
-            pass
-        else:
-            text_list.append(text)
+        for word in wakati_text:
+            if '名詞' in word.feature:
+                wakati_list.append(word.surface)
+                nouns.append(word.surface)
+            if '動詞' in word.feature:
+                verbs.append(word.surface)
+            if '形容詞' in word.feature:
+                adjs.append(word.surface)
+            if '副詞' in word.feature:
+                advs.append(word.surface)
 
-    text_all += "".join(text_list)
+        # send wakati_all to word_cloud route
+        #global wakati_all
+        wakati_all = " ".join(wakati_list)
+
+        fpath = "Fonts/NotoSansCJKjp-Medium.otf"
+        d = path.dirname(__file__)
+        alice_mask = np.array(Image.open(path.join(d, "alice_mask.png")))
     
-    # keitaiso bunseki
-    tagger = Tagger()
-    wakati_text = tagger.parse(text_all)
-
-    for word in wakati_text:
-        if '名詞' in word.feature:
-            wakati_list.append(word.surface)
-            nouns.append(word.surface)
-        if '動詞' in word.feature:
-            verbs.append(word.surface)
-        if '形容詞' in word.feature:
-            adjs.append(word.surface)
-        if '副詞' in word.feature:
-            advs.append(word.surface)
-
-    # send wakati_all to word_cloud route
-    #global wakati_all
-    wakati_all = " ".join(wakati_list)
-
-    fpath = "Fonts/NotoSansCJKjp-Medium.otf"
-    d = path.dirname(__file__)
-    alice_mask = np.array(Image.open(path.join(d, "alice_mask.png")))
+        wakati_all = "テスト中 "
+        wakati_all += session.get('wakati_all', None)
     
-    wakati_all = "テスト中 "
-    wakati_all += session.get('wakati_all', None)
-    
-    stop_words = [
-        u'こと', u'そう', u'はず', u'みたい', u'それ',
-        u'よう', u'こと', u'これ', u'ため', u'せい', 
-        u'どころ'
-    ]
+        stop_words = [
+            u'こと', u'そう', u'はず', u'みたい', u'それ',
+            u'よう', u'こと', u'これ', u'ため', u'せい', 
+            u'どころ'
+        ]
 
-    wordcloud = WordCloud(
-        background_color = 'white',
-        max_font_size = 40,
-        relative_scaling = .5,
-        font_path = fpath,
-        stopwords = set(stop_words),
-        mask = alice_mask,
-        ).generate(wakati_all)
+        wordcloud = WordCloud(
+            background_color = 'white',
+            max_font_size = 40,
+            relative_scaling = .5,
+            font_path = fpath,
+            stopwords = set(stop_words),
+            mask = alice_mask,
+            ).generate(wakati_all)
            
-    fig = plt.figure()
-    plt.imshow(wordcloud)
-    plt.axis("off")
+        fig = plt.figure()
+        plt.imshow(wordcloud)
+        plt.axis("off")
         
-    img = io.BytesIO()
-    fig.savefig(img)
-    img.seek(0)
-    response = send_file(img, mimetype='image/png')
-    return response
+        img = io.BytesIO()
+        fig.savefig(img)
+        img.seek(0)
+        response = send_file(img, mimetype='image/png')
+        return response
 
 # Set auth page
 @app.route('/twitter_auth', methods=['GET'])
