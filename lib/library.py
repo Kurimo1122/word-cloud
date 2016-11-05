581b53256e7dcfde52aa25d8723456502f6796c3
Kurimo1122 <kugimoto1122@gmail.com>
2016-11-05 19:02:07 +0900

    Modified the error in ave_hinshi_list

--- lib/library.py
+++ lib/library.py
@@ -28,27 +28,27 @@ def user_timeline():
 def save_hinshi_list(nounswords, verbswords, adjswords, advswords, 
     nounspoint, verbspoint, adjspoint, advspoint):
     with open('pn_ja.dic.txt', 'r', encoding="Shift-JIS") as f:
-    it = (line for line in f)
-    while True:
-        try:
-            line = next(it)
-        except StopIteration:
-            break
-        line = line.rstrip()
-        x = line.split(':')
-        if abs(float(x[3])) > 0:
-            if x[2] == '名詞':
-                nounswords.append(x[0])
-                nounspoint.append(x[3])
-            if x[2] == '動詞':
-                verbswords.append(x[0])
-                verbspoint.append(x[3])
-            if x[2] == '形容詞':
-                adjswords.append(x[0])
-                adjspoint.append(x[3])
-            if x[2] == '副詞':
-                advswords.append(x[0])
-                advspoint.append(x[3])
+        it = (line for line in f)
+        while True:
+            try:
+                line = next(it)
+            except StopIteration:
+                break
+            line = line.rstrip()
+            x = line.split(':')
+            if abs(float(x[3])) > 0:
+                if x[2] == '名詞':
+                    nounswords.append(x[0])
+                    nounspoint.append(x[3])
+                if x[2] == '動詞':
+                    verbswords.append(x[0])
+                    verbspoint.append(x[3])
+                if x[2] == '形容詞':
+                    adjswords.append(x[0])
+                    adjspoint.append(x[3])
+                if x[2] == '副詞':
+                    advswords.append(x[0])
+                    advspoint.append(x[3])
 
 #get analyzed tweets text
 def get_tweet_keitaiso_kaiseki(timeline, text_list, text_all):
